48779f08bcf371453f9b9c94ede733f426ae4496
GreattitJY <great.tit.jaejae@gmail.com>
2025-01-14 17:41:25 +0900

    chore: edit blog title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Greattit Blog";
+export const SITE_TITLE = "GreattitJY Blog";
 export const SITE_DESCRIPTION = "Welcome to my dev blog";
